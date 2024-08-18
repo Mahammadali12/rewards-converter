cefa25496e54d9ac5277161ce36abc48579b8f6e
Mahammadali12 <zamanli.mehemmedeli@gmail.com>
2024-08-17 23:21:58 -0400

    fisnihed

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,22 @@
+public class RewardValue {
+    double cash;
+    public RewardValue (double cash){
+        this.cash=cash;
+    }
+
+    public RewardValue (int  miles){
+        this.cash = convertToCash(miles);
+    }
+
+    int convertToMiles(double cash){
+        return (int) (cash/0.0035);
+    }
+
+    double convertToCash(int miles){
+        return miles*0.0035;
+    }
+
+    int getMilesValue(){
+        return (int) (this.cash/0.0035);
+    }
+}
